387d661b53ef70916aadc7d597ff3c2aca7f0182
Jakub Dziurgot <j.dziurgot@heating.pl>
2022-12-03 21:18:57 +0100

    Add all operations

--- calculator.py
+++ calculator.py
@@ -15,3 +15,22 @@ choice = input("Podaj działanie, posługując się odpowiednią liczbą: 1.Doda
 
 num1 = float(input ("Podaj składnik 1: "))    
 num2 = float(input ("Podaj składnik 2: "))
+
+if choice == '1':
+    add_result = add(num1,num2)
+    logging.info("Dodaję {} i {}".format(num1, num2))
+    logging.info("Wynik to: {}".format(add_result))      
+elif choice == '2':
+    sub_result = subtract(num1,num2)
+    logging.info("Dodaję {} i {}".format(num1, num2))
+    logging.info("Wynik to: {}".format(sub_result))    
+elif choice == '3':
+    multi_result = multiply(num1,num2)
+    logging.info("Dodaję {} i {}".format(num1, num2))
+    logging.info("Wynik to: {}".format(multi_result))       
+elif choice == '4':
+    div_result = divide(num1,num2)
+    logging.info("Dodaję {} i {}".format(num1, num2))
+    logging.info("Wynik to: {}".format(div_result))       
+else:    
+    logging.error("Podaj liczbę!")
