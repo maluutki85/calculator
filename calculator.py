1d95d067e79b2522399cf8935bf988347bbc5a06
Jakub Dziurgot <j.dziurgot@heating.pl>
2022-12-04 14:37:50 +0100

    Change looging to print

--- calculator.py
+++ calculator.py
@@ -19,18 +19,18 @@ num2 = float(input ("Podaj składnik 2: "))
 if choice == '1':
     add_result = add(num1,num2)
     logging.info("Dodaję {} i {}".format(num1, num2))
-    logging.info("Wynik to: {}".format(add_result))      
+    print("Wynik to: {}".format(add_result))      
 elif choice == '2':
     sub_result = subtract(num1,num2)
     logging.info("Dodaję {} i {}".format(num1, num2))
-    logging.info("Wynik to: {}".format(sub_result))    
+    print("Wynik to: {}".format(sub_result))    
 elif choice == '3':
     multi_result = multiply(num1,num2)
     logging.info("Dodaję {} i {}".format(num1, num2))
-    logging.info("Wynik to: {}".format(multi_result))       
+    print("Wynik to: {}".format(multi_result))       
 elif choice == '4':
     div_result = divide(num1,num2)
     logging.info("Dodaję {} i {}".format(num1, num2))
-    logging.info("Wynik to: {}".format(div_result))       
+    print("Wynik to: {}".format(div_result))       
 else:    
-    logging.error("Podaj liczbę!")
+    print("Podaj liczbę!")
