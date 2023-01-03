4c0db7199854c23fde1eebe23be6a95b615a0821
Jakub Dziurgot <j.dziurgot@heating.pl>
2023-01-03 19:30:13 +0100

    Add main func

--- calculator.py
+++ calculator.py
@@ -1,44 +1,47 @@
 import logging
 
-logging.basicConfig(format='%(message)s', level=logging.INFO)
+
+if __name__ == "__main__":
+    pass
+    logging.basicConfig(format='%(message)s', level=logging.INFO)
 
 # This function add two numbers
-def add(x, y):  
-   return x + y
+    def add(x, y):  
+        return x + y
 
 # This function subtracts two numbers
-def subtract(x, y):  
-   return x - y
+    def subtract(x, y):  
+        return x - y
 
 # This function multiply two numbers     
-def multiply(x, y):   
-   return x * y   
+    def multiply(x, y):   
+        return x * y   
 
 # This divide multiply two numbers 
-def divide(x, y):      
-   return x / y
+    def divide(x, y):      
+        return x / y
 
 # Take input from user
-choice = input("Podaj działanie, posługując się odpowiednią liczbą: 1.Dodawanie, 2.Odejmowanie, 3.Mnożenie, 4.Dzielenie: ")
-num1 = float(input ("Podaj składnik 1: "))    
-num2 = float(input ("Podaj składnik 2: "))
+    choice = input("Podaj działanie, posługując się odpowiednią liczbą: 1.Dodawanie, 2.Odejmowanie, 3.Mnożenie, 4.Dzielenie: ")
+    num1 = float(input("Podaj składnik 1: "))    
+    num2 = float(input("Podaj składnik 2: "))
 
 # Check choice from user
-if choice == '1':
-    logging.info("Dodaję {} i {}".format(num1, num2))
-    print("Wynik to:", add(num1,num2))
+    if choice == '1':
+        logging.info("Dodaję {} i {}".format(num1, num2))
+        print("Wynik to:", add(num1,num2))
 
-elif choice == '2':
-    logging.info("Odejmuję {} i {}".format(num1, num2))
-    print("Wynik to:", subtract(num1,num2)) 
+    elif choice == '2':
+        logging.info("Odejmuję {} i {}".format(num1, num2))
+        print("Wynik to:", subtract(num1,num2)) 
 
-elif choice == '3':
-    logging.info("Mnożę {} i {}".format(num1, num2))
-    print("Wynik to:", multiply(num1,num2)) 
+    elif choice == '3':
+        logging.info("Mnożę {} i {}".format(num1, num2))
+        print("Wynik to:", multiply(num1,num2)) 
 
-elif choice == '4':
-    logging.info("Dzielę {} i {}".format(num1, num2))
-    print("Wynik to:", divide(num1,num2))  
-    
-else:    
-    print("Podaj liczbę!")
+    elif choice == '4':
+        logging.info("Dzielę {} i {}".format(num1, num2))
+        print("Wynik to:", divide(num1,num2))  
+
+    else:    
+        print("Podaj liczbę!")
